f2efc338871606b64e07b7e754425064504e6c7f
brit1172 <baparicio.16@gmail.com>
2020-08-10 19:00:32 -0400

    Add files via upload

---
+++ ToDoListProj/toDoListProj/toDoListProj/ToDo.swift
@@ -0,0 +1,12 @@
+//
+//  ToDo.swift
+//  toDoListProj
+//
+//  Created by Britney Aparicio on 8/10/20.
+//  Copyright © 2020 Britney Aparicio. All rights reserved.
+//
+
+class ToDoClass{
+    var description = " "
+    var important = false
+}
---
+++ ToDoListProj/toDoListProj/toDoListProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toDoListProj
+//
+//  Created by Britney Aparicio on 8/10/20.
+//  Copyright © 2020 Britney Aparicio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
